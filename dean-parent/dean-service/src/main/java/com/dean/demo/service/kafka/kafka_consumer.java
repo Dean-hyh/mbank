f335a20bf59f23fe812b8e2c010728aed73755e7
houyuanhui <houyuanhui@yitong.com.cn>
2021-07-18 17:43:17 +0800

    20210718 常规更新

---
+++ dean-parent/dean-service/src/main/java/com/dean/demo/service/kafka/kafka_consumer.java
@@ -0,0 +1,32 @@
+package com.dean.demo.service.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class kafka_consumer {
+    public static void main(String[] args) throws Exception {
+        String topicName = "mapp_star_dream";
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.230.129:9092");
+        props.put("group.id", "star_dream");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        //Kafka Consumer subscribes list of topics here.
+        consumer.subscribe(Arrays.asList(topicName));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s\n",
+                        record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
